580b368072697185a8a5eef888c58e7dccc364a0
Mahesh <mahesh.g@live.com>
2020-09-01 01:13:07 +0530

    Moved to zsh Updated initial list of vs extensions Updated scripts

---
+++ linux-installer.sh
@@ -0,0 +1,18 @@
+#Installations
+
+# Fish shell
+add-apt-repository ppa:git-core/ppa
+apt update 
+apt-get install git
+
+# Meld
+apt-get install meld
+
+# Visual source code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt-get update
+apt-get install code # or code-insiders
+
+cat ./initial-vs-code-extensions.txt | xargs code --install-extension
