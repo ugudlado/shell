c1725269c6fc142027cd99c1b827f958f959d5e4
Mahesh <mahesh.g@live.com>
2021-09-04 17:51:35 +0530

    Added nvm setup

--- .profile
+++ .profile
@@ -21,6 +21,14 @@ if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
 fi
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 # aliases
 alias gst='git status'
 alias gp='git pull'
--- installers/ubuntu-installer.sh
+++ installers/ubuntu-installer.sh
@@ -4,7 +4,7 @@
 add-apt-repository ppa:git-core/ppa
 apt update 
 apt-get install git
-
+ 
 # Visual source code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
@@ -13,5 +13,8 @@ apt-get update
 apt-get install code # or code-insiders
 
 apt-get install zsh fzf direnv asciinema meld
+
+# install nvm for nodejs version manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 ../shell-setup.sh
